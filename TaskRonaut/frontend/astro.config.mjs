facd2d1faf9ccfbc2c107697ee220979340602d6
paul <paul.schreiber@it.htlhl.at>
2025-02-20 19:51:04 +0100

    update1

--- TaskRonaut/frontend/astro.config.mjs
+++ TaskRonaut/frontend/astro.config.mjs
@@ -1,13 +1,15 @@
-import {defineConfig} from 'astro/config';
-import node from "@astrojs/node";
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
 export default defineConfig({
-    output: 'server',
-    adapter: node({ mode: "standalone" }),
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
+  vite: {
     server: {
-        host: true,
-        port: 4321,
-        strictPort: true,
-        allowedHosts: ['taskronaut.at'],
+      host: true,
+      port: 4321,
+      strictPort: true,
+      allowedHosts: ['taskronaut.at'],
     },
+  },
 });
