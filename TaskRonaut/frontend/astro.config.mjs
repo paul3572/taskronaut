0f5208ba5b6e92fe8b788d003276c01dbd98a79b
Lucas <lucasobst29@gmail.com>
2025-02-21 16:55:27 +0100

    AAAAAAAAAAAAH

--- TaskRonaut/frontend/astro.config.mjs
+++ TaskRonaut/frontend/astro.config.mjs
@@ -1,13 +1,10 @@
-import {defineConfig} from 'astro/config';
-import node from "@astrojs/node";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    output: 'server',
-    adapter: node({mode: "standalone"}),
     server: {
         host: true,
         port: 4321,
         strictPort: true,
-        allowedHosts: ['localhost','taskronaut.at'],
+        allowedHosts: ['localhost', 'taskronaut.at'],
     }
 });
