a78aab803761a1b01c8122f2f3ddb3ff275124ef
paul3572 <paulpaulpaul3572@gmail.com>
2025-02-28 12:40:57 +0100

    bugfix

--- TaskRonaut/backend/src/middleware/errorHandler.js
+++ TaskRonaut/backend/src/middleware/errorHandler.js
@@ -16,57 +16,73 @@ import {
     UserNotFoundError
 } from "./errors.mjs";
 
-
 export async function errorHandler(error) {
+    const errorMessage = `ERROR: ${error.message}`;
+    const errorDetails = `Error Name: ${error.name}, Stack: ${error.stack}`;
+
     if (error instanceof UserNotFoundError) {
-        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
-        return {statusCode: 500};
+        logger.error(chalk.hex(styles.critical)`${errorMessage}`);
+        logger.debug(chalk.hex(styles.debug)`${errorDetails}`);
+        return {statusCode: 404};
     } else if (error instanceof NoBoardsFoundError) {
-        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
-        return {statusCode: 500};
+        logger.error(chalk.hex(styles.critical)`${errorMessage}`);
+        logger.debug(chalk.hex(styles.debug)`${errorDetails}`);
+        return {statusCode: 404};
     } else if (error instanceof BoardNotFoundError) {
-        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
-        return {statusCode: 500};
+        logger.error(chalk.hex(styles.critical)`${errorMessage}`);
+        logger.debug(chalk.hex(styles.debug)`${errorDetails}`);
+        return {statusCode: 404};
     } else if (error instanceof QueryExecutionError) {
         logger.error(chalk.hex(styles.critical)`DATABASE ERROR: ${error.message}`);
+        logger.debug(chalk.hex(styles.debug)`${errorDetails}`);
         return {statusCode: 500};
     } else if (error instanceof ActivationError) {
-        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
-        return {statusCode: 500};
+        logger.error(chalk.hex(styles.critical)`${errorMessage}`);
+        logger.debug(chalk.hex(styles.debug)`${errorDetails}`);
+        return {statusCode: 400};
     } else if (error instanceof DatabaseError) {
         logger.error(chalk.hex(styles.critical)`DATABASE ERROR: ${error.message}`);
+        logger.debug(chalk.hex(styles.debug)`${errorDetails}`);
         return {statusCode: 500};
     } else if (error instanceof PermissionDeniedError) {
-        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
+        logger.error(chalk.hex(styles.critical)`${errorMessage}`);
+        logger.debug(chalk.hex(styles.debug)`${errorDetails}`);
         return {statusCode: 403};
     } else if (error instanceof InvalidTokenError) {
-        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
+        logger.error(chalk.hex(styles.critical)`${errorMessage}`);
+        logger.debug(chalk.hex(styles.debug)`${errorDetails}`);
         return {statusCode: 401};
     } else if (error instanceof InvalidSessionError) {
-        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
+        logger.error(chalk.hex(styles.critical)`${errorMessage}`);
+        logger.debug(chalk.hex(styles.debug)`${errorDetails}`);
         return {statusCode: 401};
     } else if (error instanceof InvalidInputError) {
-        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
+        logger.error(chalk.hex(styles.critical)`${errorMessage}`);
+        logger.debug(chalk.hex(styles.debug)`${errorDetails}`);
         return {statusCode: 400};
     } else if (error instanceof InvalidLoginDataError) {
-        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
+        logger.error(chalk.hex(styles.critical)`${errorMessage}`);
+        logger.debug(chalk.hex(styles.debug)`${errorDetails}`);
         return {statusCode: 401};
     } else if (error instanceof EmailAlreadyInUseError) {
-        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
+        logger.error(chalk.hex(styles.critical)`${errorMessage}`);
+        logger.debug(chalk.hex(styles.debug)`${errorDetails}`);
         return {statusCode: 409};
-    }
-    else if (error instanceof UserIsAlreadyMemberError) {
-        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
-        return {statusCode: 401};
-    }
-    else if (error instanceof ActivationTokenNotFoundError) {
-        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
+    } else if (error instanceof UserIsAlreadyMemberError) {
+        logger.error(chalk.hex(styles.critical)`${errorMessage}`);
+        logger.debug(chalk.hex(styles.debug)`${errorDetails}`);
+        return {statusCode: 409};
+    } else if (error instanceof ActivationTokenNotFoundError) {
+        logger.error(chalk.hex(styles.critical)`${errorMessage}`);
+        logger.debug(chalk.hex(styles.debug)`${errorDetails}`);
         return {statusCode: 404};
     } else if (error instanceof DatabaseConnectionError) {
         logger.error(chalk.hex(styles.critical)`DATABASE CONNECTION ERROR: ${error.message}`);
+        logger.debug(chalk.hex(styles.debug)`${errorDetails}`);
         return {statusCode: 500};
     } else {
         logger.error(chalk.hex(styles.critical)`UNHANDLED ERROR: ${JSON.stringify(error)}`);
+        logger.debug(chalk.hex(styles.debug)`${errorDetails}`);
         return {statusCode: 500};
     }
 }
