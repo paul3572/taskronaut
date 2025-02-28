692db610ae68dbf14038bb5e1a4ded6b65e3a458
paul3572 <paulpaulpaul3572@gmail.com>
2025-02-28 01:46:04 +0100

    errors zentral verwalten

---
+++ TaskRonaut/backend/src/middleware/errorHandler.js
@@ -0,0 +1,67 @@
+import logger from "./logger.mjs";
+import chalk from "chalk";
+import {styles} from "../database/loggingStyle.mjs";
+import {
+    ActivationError, ActivationTokenNotFoundError,
+    BoardNotFoundError, DatabaseConnectionError,
+    DatabaseError,
+    EmailAlreadyInUseError,
+    InvalidInputError,
+    InvalidLoginDataError,
+    InvalidSessionError,
+    InvalidTokenError,
+    NoBoardsFoundError,
+    PermissionDeniedError,
+    QueryExecutionError,
+    UserNotFoundError
+} from "./errors.mjs";
+
+
+export async function errorHandler(error) {
+    if (error instanceof UserNotFoundError) {
+        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
+        return {statusCode: 500};
+    } else if (error instanceof NoBoardsFoundError) {
+        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
+        return {statusCode: 500};
+    } else if (error instanceof BoardNotFoundError) {
+        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
+        return {statusCode: 500};
+    } else if (error instanceof QueryExecutionError) {
+        logger.error(chalk.hex(styles.critical)`DATABASE ERROR: ${error.message}`);
+        return {statusCode: 500};
+    } else if (error instanceof ActivationError) {
+        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
+        return {statusCode: 500};
+    } else if (error instanceof DatabaseError) {
+        logger.error(chalk.hex(styles.critical)`DATABASE ERROR: ${error.message}`);
+        return {statusCode: 500};
+    } else if (error instanceof PermissionDeniedError) {
+        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
+        return {statusCode: 403};
+    } else if (error instanceof InvalidTokenError) {
+        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
+        return {statusCode: 401};
+    } else if (error instanceof InvalidSessionError) {
+        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
+        return {statusCode: 401};
+    } else if (error instanceof InvalidInputError) {
+        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
+        return {statusCode: 400};
+    } else if (error instanceof InvalidLoginDataError) {
+        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
+        return {statusCode: 401};
+    } else if (error instanceof EmailAlreadyInUseError) {
+        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
+        return {statusCode: 409};
+    } else if (error instanceof ActivationTokenNotFoundError) {
+        logger.error(chalk.hex(styles.critical)`ERROR: ${error.message}`);
+        return {statusCode: 404};
+    } else if (error instanceof DatabaseConnectionError) {
+        logger.error(chalk.hex(styles.critical)`DATABASE CONNECTION ERROR: ${error.message}`);
+        return {statusCode: 500};
+    } else {
+        logger.error(chalk.hex(styles.critical)`UNHANDLED ERROR: ${JSON.stringify(error)}`);
+        return {statusCode: 500};
+    }
+}
---
+++ TaskRonaut/backend/src/middleware/errors.mjs
@@ -0,0 +1,112 @@
+export class DatabaseConnectionError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "DatabaseConnectionError";
+    }
+}
+
+export class UserNotFoundError extends Error {
+    constructor(userId) {
+        super(`Benutzer mit der ID ${userId} wurde nicht gefunden.`);
+        this.name = "UserNotFoundError";
+        this.userId = userId;
+    }
+}
+
+export class NoBoardsFoundError extends Error {
+    constructor(userId) {
+        super(`Keine Boards für den Benutzer mit der ID ${userId} gefunden.`);
+        this.name = "NoBoardsFoundError";
+        this.userId = userId;
+    }
+}
+
+export class InvalidBoardMemberDataError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "InvalidBoardMemberDataError";
+    }
+}
+
+export class BoardNotFoundError extends Error {
+    constructor(boardId) {
+        super(`Board mit der ID ${boardId} wurde nicht gefunden.`);
+        this.name = "BoardNotFoundError";
+        this.boardId = boardId;
+    }
+}
+
+export class QueryExecutionError extends Error {
+    constructor(query, params, originalError) {
+        super(`Fehler bei der Ausführung der Abfrage: ${query}. Parameter: ${JSON.stringify(params)}. Ursprünglicher Fehler: ${originalError.message}`);
+        this.name = "QueryExecutionError";
+        this.query = query;
+        this.params = params;
+        this.originalError = originalError;
+    }
+}
+
+export class ActivationError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "ActivationError";
+    }
+}
+
+export class DatabaseError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "DatabaseError";
+    }
+}
+
+export class PermissionDeniedError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "PermissionDeniedError";
+    }
+}
+
+export class InvalidTokenError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "InvalidTokenError";
+    }
+}
+
+export class InvalidSessionError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "InvalidSessionError";
+    }
+}
+
+export class InvalidInputError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "InvalidInputError";
+    }
+}
+
+export class InvalidLoginDataError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "InvalidLoginDataError";
+    }
+}
+
+export class EmailAlreadyInUseError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "EmailAlreadyInUseError";
+    }
+}
+
+export class ActivationTokenNotFoundError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "ActivationTokenNotFoundError";
+    }
+}
+
+
