b1de328e68fa5505be224580263f2a85be0b5498
paul3572 <paulpaulpaul3572@gmail.com>
2025-02-28 15:54:28 +0100

    bugfix

--- TaskRonaut/backend/src/database/preparedStatements/psTask.mjs
+++ TaskRonaut/backend/src/database/preparedStatements/psTask.mjs
@@ -55,11 +55,11 @@ class PsTask {
     }
     async selectBoardIdByTask(taskId) {
         const [boardId] = await dbConnection.query(taskQueries.getBoardIdFromTaskById, [taskId]);
-        console.log("BOARD ID FROM TASK:"+JSON.stringify(boardId));
+        console.log("BOARD ID FROM TASK:"+JSON.stringify(boardId[0].boardID));
         console.log("Länge: "+tasks.length);
         if (tasks.length === 0) {
             logger.info('not found');
-            throw new Error("Task not found");
+            //throw new Error("Task not found");
         }
         return boardId[0].boardID;
     }
