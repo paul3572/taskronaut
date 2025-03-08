a1fe5f8c4f010a8b4c0252d40ab6af2c1b6daa71
paul3572 <paulpaulpaul3572@gmail.com>
2025-03-08 22:12:58 +0100

    server erreichen

--- TaskRonaut/frontend/src/pages/conf.js
+++ TaskRonaut/frontend/src/pages/conf.js
@@ -4,7 +4,8 @@ const clientHostName = 'localhost';
 const serverPort = 3001;
 const clientPort = 4321;
 
-const protocol = 'http';
+const serverProtocol = 'https';
+const clientProtocol = 'http';
 
-export const serverURL = `${protocol}://${serverHostName}`;
-export const clientURL = `${protocol}://${clientHostName}:${clientPort}`;
+export const serverURL = `${serverProtocol}://${serverHostName}`;
+export const clientURL = `${clientProtocol}://${clientHostName}:${clientPort}`;
