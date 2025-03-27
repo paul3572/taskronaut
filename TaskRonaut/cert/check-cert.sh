b3ffe6d88ac9bb0141b2b4c5781e4fe656b194c3
paul3572 <paulpaulpaul3572@gmail.com>
2025-03-27 12:35:48 +0100

    überprüfung auf richtigkeit

--- TaskRonaut/cert/check-cert.sh
+++ TaskRonaut/cert/check-cert.sh
@@ -8,7 +8,7 @@ expiry_date=$(certbot certificates | grep "Expiry Date:" | awk '{print $4" "$5}'
 echo "Ablaufdatum: $expiry_date"
 
 # Aktuelles Datum in 60 Tagen berechnen
-check_date=$(date -d "$expiry_date -60 days" +'%Y-%m-%d')
+check_date=$(date -d "$(echo $expiry_date) -60 days" +'%Y-%m-%d')
 echo "Checkdatum: $check_date"
 
 # Aktuelles Datum abrufen
@@ -20,5 +20,5 @@ if [ "$current_date" \< "$check_date" ]; then
   echo "Zertifikat ist gültig."
 else
   echo "neues Zertifikat wird erstellt"
-  certbot renew --webroot -w /var/www/certbot --force-renewal --email paul.schreiber@it.htl-hl.ac.at -d taskronaut.at --agree-tos
+  # certbot renew --webroot -w /var/www/certbot --force-renewal --email paul.schreiber@it.htl-hl.ac.at -d taskronaut.at --agree-tos
 fi
