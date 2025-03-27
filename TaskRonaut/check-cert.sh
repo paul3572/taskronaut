2f6535895b81fc94781b62ecbd741550d4cd1791
paul3572 <paulpaulpaul3572@gmail.com>
2025-03-27 11:18:39 +0100

    nicht immer neues zertifikat

---
+++ TaskRonaut/check-cert.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if certbot certificates | grep "Expiry Date: $(date -d "+60 days" +'%Y-%m-%d')"; then
+  echo "Zertifikat ist gültig."
+else
+  certbot renew --webroot -w /var/www/certbot --force-renewal --email paul.schreiber@it.htl-hl.ac.at -d taskronaut.at --agree-tos
+fi
