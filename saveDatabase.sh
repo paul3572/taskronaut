2c0649b319257dd8d6195b6c373a8616cbe0526d
paul3572 <paulpaulpaul3572@gmail.com>
2025-03-27 20:20:39 +0100

    script test

--- saveDatabase.sh
+++ saveDatabase.sh
@@ -16,8 +16,10 @@ echo "Backup-Datei wurde gelöscht."
 
 if [ -n "$db_password" ]; then
     docker exec -i $db_container_name mysqldump -u "$db_user" -p"$db_password" --databases "$db_name" > "$backup_file"
+    echo "mit Passwort"
 else
     docker exec -i $db_container_name mysqldump -u "$db_user" --databases "$db_name" > "$backup_file"
+    echo "ohne Passwort"
 fi
 
 echo "Backup gespeichert als $backup_file"
