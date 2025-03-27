8aa12a4e2ed180aa9f8fc1e5788af73bb42b5da6
paul3572 <paulpaulpaul3572@gmail.com>
2025-03-27 20:16:39 +0100

    script env

--- saveDatabase.sh
+++ saveDatabase.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-db_container_name="datenbank"
-db_user="root"
-db_name="TaskRonaut"
+ENV_FILE="./backend/.env"
+if [ -f "$ENV_FILE" ]; then
+    export $(grep -v '^#' "$ENV_FILE" | xargs)
+fi
+
+db_container_name="${MYSQL_HOST}"
+db_user="${MYSQL_USER}"
+db_password="${MYSQL_PASSWORD}"
+db_name="${MYSQL_DATABASE}"
 backup_file="backup.sql"
 
 rm $backup_file
 echo "Backup-Datei wurde gelöscht."
 
-docker exec -i $db_container_name mysqldump -u $db_user --databases $db_name > $backup_file
+if [ -n "$db_password" ]; then
+    docker exec -i $db_container_name mysqldump -u "$db_user" -p"$db_password" --databases "$db_name" > "$backup_file"
+else
+    docker exec -i $db_container_name mysqldump -u "$db_user" --databases "$db_name" > "$backup_file"
+fi
 
 echo "Backup gespeichert als $backup_file"
